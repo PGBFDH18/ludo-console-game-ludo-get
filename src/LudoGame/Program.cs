7e916c64512b6ba47d01ad7cf73b7b1ce8dae6f3
FatlumIbishi <fatlum.ibishi@gmail.com>
2019-01-08 15:16:13 +0100

    Max 4, Min 2 Players

--- src/LudoGame/Program.cs
+++ src/LudoGame/Program.cs
@@ -5,11 +5,26 @@ namespace LudoGame
 {
     class Program
     {
+        static int numberOfPlayers;
+
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Ludo!");
-            Console.WriteLine("Number of players:");
-            int numberOfPlayers = int.Parse(Console.ReadLine());
+
+            while (numberOfPlayers <= 1 || numberOfPlayers >= 5)
+            {
+                Console.WriteLine("Number of players:");
+                numberOfPlayers = int.Parse(Console.ReadLine());
+
+                if (numberOfPlayers <= 1)
+                {
+                    Console.WriteLine("Must be more than 2 players");
+                }
+                else if (numberOfPlayers >= 5)
+                {
+                    Console.WriteLine("Maximum 4 players");
+                }
+            }
             var game = new GameEngine.LudoGame();
             game.StartGame(numberOfPlayers);
         }
