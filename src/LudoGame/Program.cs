a70a5e4873ae2286cc83be74ed950f8e02f3368b
FkkLagom <larsson.benjamin@live.se>
2019-01-10 15:30:15 +0100

    Update Program.cs

--- src/LudoGame/Program.cs
+++ src/LudoGame/Program.cs
@@ -7,14 +7,30 @@ namespace LudoGame
 {
     internal class Program
     {
+        private static List<string> colors = new List<string> { "Red", "Blue", "Green", "Yellow" };
         private static int numberOfPlayers;
+        private static string[][] players = new string[numberOfPlayers][];
 
         private static void Main(string[] args)
         {
+            PrintWellcomeText();
+            SetNumberOfPlayers();
+            SetPlayers();
+            //var game = new GameEngine.LudoGame(players);
+            //  game.StartGame(players);
+        }
+
+        private static void PrintWellcomeText()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
             var logo = File.ReadAllLines(@"Logo.txt");
             foreach (var item in logo)
                 Console.WriteLine(item);
+        }
 
+        private static void SetNumberOfPlayers()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
             while (numberOfPlayers < 2 || numberOfPlayers > 4)
             {
                 Console.WriteLine("Input numbers of players:");
@@ -23,27 +39,24 @@ namespace LudoGame
                     numberOfPlayers = int.Parse(Console.ReadLine());
 
                     if (numberOfPlayers < 2)
-                    {
                         Console.WriteLine("Must be more atleast 2 players");
-                    }
                     else if (numberOfPlayers > 4)
-                    {
                         Console.WriteLine("Maximum 4 players");
-                    }
                 }
                 catch (Exception)
                 {
                     Console.WriteLine("Number of players must be a digit");
                 }
             }
+        }
 
-            var colors = new List<string> { "Red", "Blue", "Green", "Yellow" };
-            var players = new string[numberOfPlayers][];
+        private static void SetPlayers()
+        {
             for (int i = 0; i < numberOfPlayers; i++)
             {
                 Console.WriteLine("\r\nInput player{0} name:", i + 1);
                 var name = Console.ReadLine();
-                Console.WriteLine("Please select color");
+                Console.WriteLine("\r\nPlease select player{0} color", i + 1);
                 for (int j = 0; j < colors.Count; j++)
                 {
                     Console.WriteLine("{0}. {1}", j + 1, colors[j]);
@@ -59,9 +72,6 @@ namespace LudoGame
                     Console.WriteLine("Selected color must be a digit.");
                 }
             }
-
-            var game = new GameEngine.Session(players);
-            //game.StartGame(numberOfPlayers, players);
         }
     }
 }
