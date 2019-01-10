f7ac78bc0da1f178cc79d8e7994204cd1dac254a
Rapidcognition <37350380+Rapidcognition@users.noreply.github.com>
2019-01-10 14:09:11 +0100

    Creating players in LudoGame.

--- src/GameEngine/LudoGame.cs
+++ src/GameEngine/LudoGame.cs
@@ -4,12 +4,28 @@ namespace GameEngine
 {
     public class LudoGame
     {
+
+        Player player = new Player();
         // Ta emot information från LudoGame library 
-        public LudoGame()
+        public LudoGame(string[][] players)
         {
             // Extrahera informationen från parametern som Session tar emot.
             // Skapar spelare
             // Skapa pjäser för respektive spelare
+            int len = players.Length;
+
+            for (int i = 0; i < len; i++)
+            {
+                string name = players[i][0];
+                string color = players[i][1];
+
+
+                player = new Player
+                {
+                    Name = name,
+                    Color = color
+                };
+            }
 
             Run();
         }
@@ -22,9 +38,6 @@ namespace GameEngine
         public void StartGame(int numberOfPlayers)
         {
             Console.WriteLine($"Game started with {numberOfPlayers} players");
-
-
-
         }
     }
 }
